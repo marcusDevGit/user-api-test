264c6911ac6176f9d659988ee6621688be791b8c
Marcus Phellypp <marcus_psc@hotmail.com>
2025-03-30 22:29:41 -0300

    🐛 Corrigido método index para tratar corretamente os dados retornados

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,83 +1,40 @@
 //api-/controllers/userController.js
 import { Sequelize } from "sequelize";
 import db from "../src/models/index.js"; // Importa o banco de dados
-import e from "express";
+import sucessResponses from "./responses/sucess.responses.js";
+import invalidResponses from "./responses/invalid.responses.js";
+import errorResponses from "./responses/error.responses.js";
 
 const { Op } = Sequelize; // Importa os operadores do Sequelize
 const { User } = db; // Desestrutura o modelo de usuário do banco de dados
 
+
 class UserController {
     async index(req, res, next) {
         try {
-            const limit = req.query.limit ? parseInt(req.query.limit) : 20;
-            const offset = req.query.offset ? parseInt(req.query.offset) : 0;
-            let where = {};
-            if (req.query.name) where.name = {
-                [Op.like]: `%${req.query.name}%`,
-            }
-            if (req.query.email) where.email = q.query.email;
-
-            const entity = await User.findAndCountAll({
-                where,
-                limit: limit,
-                offset: offset,
-                order: [['created_at', 'DESC']],
-            });
-
-            return res.status(200).json({
-                status: 200,
-                message: "Lista de usuários",
-                data: entity.rows,
-                count: entity.count,
-            });
+            const { entity, meta } = await User.seach(req.query)
+            return sucessResponses(res, 200, "Lista de usuários", entity, meta);
         } catch (error) {
-            console.error("Erro ao buscar usuários:", error);
-            return res.status(500).json({
-                status: 500,
-                message: "Erro ao buscar usuários",
-                error: error.message,
-            });
+            console.log(error)
+            return errorResponses(res, 500, "Erro ao buscar usuários", error.message);
         }
     }
     async show(req, res, next) {
         try {
-            const entity = await User.findByPk(req.params.id)
-            return res.status(200).json({
-                status: 200,
-                message: "Usuário encontrado",
-                data: entity,
-            });
+            const entity = await User.getId(req.params.id)
+            return sucessResponses(res, 200, "Usuário encontrado", entity);
 
         } catch (error) {
-            console.error("Erro ao buscar usuários:", error);
-            return res.status(500).json({
-                status: 500,
-                message: "Erro ao buscar usuários",
-                error: error.message,
-            });
+            return errorResponses(res, 500, "Erro ao buscar usuário", error.message);
         }
     }
     async create(req, res, next) {
         try {
             const entity = await User.create(req.body)
-            return res.status(201).json({
-                status: 201,
-                message: "Usuário criado com sucesso",
-                data: entity,
-            });
+            return sucessResponses(res, 201, "Usuário criado com sucesso", entity);
         } catch (error) {
             if (error.name && error.name.includes("SequelizeValidationError")) {
-                return res.status(404).json({
-                    status: INVALID,
-                    message: "Dados inválidos",
-                    error: error.errors.map(e => {
-                        return {
-                            message: e.message,
-                            field: e.path,
-                            value: e.value,
-                        }
-                    }),
-                });
+                return invalidResponses(res, 404, "Dados inválidos", error);
 
             }
             return res.status(500).json({
@@ -91,59 +48,37 @@ class UserController {
         try {
             const entityOld = await User.findByPk(req.params.id)
             const entityNew = await entityOld.update(req.body)
-            return res.status(201).json({
-                status: 201,
-                message: "Usuário atualizado com sucesso",
-                data: entityNew,
-            });
+            return sucessResponses(res, 200, "Usuário atualizado com sucesso", entityNew);
         } catch (error) {
             if (error.name && error.name.includes("SequelizeValidationError")) {
-                return res.status(404).json({
-                    status: INVALID,
-                    message: "Dados inválidos",
-                    error: error.errors.map(e => {
+                return invalidResponses(res, 404, "Dados inválidos", error({
+                    errors: error.errors.map(e => {
                         return {
                             message: e.message,
                             field: e.path,
                             value: e.value,
                         }
                     }),
-                });
+                }));
 
             }
-            return res.status(500).json({
-                status: 500,
-                message: "Erro ao atualizar usuário",
-                error: error.message,
-            });
+            return errorResponses(res, 500, "Erro ao atualizar usuário", error.message);
         }
     }
     async destroy(req, res, next) {
         try {
             const entityOld = await User.findByPk(req.params.id)
             if (!entityOld) {
-                return res.status(404).json({
-                    status: 404,
-                    message: "Usuário não encontrado",
-                });
+                return errorResponses(res, 404, "Usuário não encontrado", error.message);
             }
             const entity = await User.destroy({
                 where: {
                     id: req.params.id,
                 }
             })
-            return res.status(200).json({
-                status: 200,
-                message: "Usuário deletado com sucesso",
-                data: entity,
-            });
+            return sucessResponses(res, 200, "Usuário deletado com sucesso", entityOld);
         } catch (error) {
-            console.error("Erro ao buscar usuários:", error);
-            return res.status(500).json({
-                status: 500,
-                message: "Erro ao buscar usuários",
-                error: error.message,
-            });
+            return errorResponses(res, 500, "Erro ao deletar usuário", error.message);
         }
     }
 }
