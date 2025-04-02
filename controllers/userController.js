7e7d85de88c8d90acd111b11af98230cd96879b7
Marcus Phellypp <marcus_psc@hotmail.com>
2025-04-01 22:54:20 -0300

    🚀 feat: Adiciona funcionalidade de login no UserController com respostas de sucesso e erro

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 //api-/controllers/userController.js
 import ResourseController from "./resource.controller.js"; // Importa o controlador de recursos
 import db from "../src/models/index.js"
+import sucessResponses from "./responses/sucess.responses.js";
+import errorResponses from "./responses/error.responses.js";
 
 const User = db.User
 
@@ -10,6 +12,19 @@ class UserController extends ResourseController {
     constructor() {
         super();
         this.setModel(User); // Define o modelo de usuário
+    }
+
+    async login(req, res, next) {
+        try {
+            const { email, password } = req.body;
+            console.log(req.body, email, password)
+            const result = await User.verifyLogin(email, password);
+            console.log(result)
+            sucessResponses(res, 200, "Usuario autenticado com sucesso!", result)
+        } catch (error) {
+            console.log(error)
+            errorResponses(res, 500, "Não foi possivel realizar o login!")
+        }
     }
 }
 
